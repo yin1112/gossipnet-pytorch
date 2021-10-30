f33d7f0b51c3dc7e418603b64e95837fac20f9af
yin1112 <55095824+yin1112@users.noreply.github.com>
2021-10-30 11:19:29 +0800

    Update criterion.py

--- nms_net_pytorch/criterion.py
+++ nms_net_pytorch/criterion.py
@@ -7,9 +7,9 @@ from torch import nn
 
 
 class Criterion(nn.Module):
-    def __init__(self  ):
+    def __init__(self  ,device):
         super(Criterion, self).__init__()
-        self.loss = torch.nn.BCEWithLogitsLoss(reduction='none')
+        self.loss = torch.nn.BCEWithLogitsLoss(reduction='none').to(device)
 
     def forward(self , prediction_score , labels):
         return self.loss(prediction_score , labels)
